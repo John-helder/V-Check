b5351b94f4b85e0e6c3011e51425a9ff0becce89
Alessandro-NSousa <allessandro188@gmail.com>
2026-01-12 19:56:08 -0300

    Feat: Implemetação parcial do crud de Usuario(Consultor).

--- src/main/java/com/vcheck/controller/UserController.java
+++ src/main/java/com/vcheck/controller/UserController.java
@@ -1,29 +1,55 @@
 package com.vcheck.controller;
 
-import com.vcheck.dto.FornecedorRequestDTO;
-import com.vcheck.dto.FornecedorResponseDTO;
 import com.vcheck.dto.UserRequestDTO;
 import com.vcheck.dto.UserResponseDTO;
-import com.vcheck.repository.UserRepository;
 import com.vcheck.service.UserService;
 import jakarta.transaction.Transactional;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
     @PostMapping
     @Transactional
-    public ResponseEntity<UserResponseDTO> createEmpresa(@RequestBody UserRequestDTO dto){
+    public ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO dto){
         var user = this.userService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping
+    public Page<UserResponseDTO> listAll(@ParameterObject
+                                             @PageableDefault(
+                                                     size = 10, page = 0,
+                                                     sort = "nome",
+                                                     direction = Sort.Direction.ASC) Pageable pageable){
+        var users = this.userService.listAll(pageable);
+
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findBy(@PathVariable Long id){
+        var user = this.userService.findByUser(id);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteUser(@PathVariable Long id){
+        this.userService.deleteUser(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/vcheck/service/UserService.java
+++ src/main/java/com/vcheck/service/UserService.java
@@ -1,11 +1,13 @@
 package com.vcheck.service;
 
-import com.vcheck.domain.EmpresaCliente;
 import com.vcheck.domain.User;
 import com.vcheck.dto.*;
 import com.vcheck.infra.exception.ValidacaoException;
 import com.vcheck.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,5 +27,21 @@ public class UserService {
 
         return new UserResponseDTO(usuarioCadastrado);
 
+    }
+
+    public Page<UserResponseDTO> listAll(Pageable pageable) {
+        return this.userRepository.findAll(pageable).map(UserResponseDTO::new);
+    }
+
+    public UserResponseDTO findByUser(Long id) {
+        var user = this.userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
+
+        return new UserResponseDTO(user);
+    }
+
+    public void deleteUser(Long id) {
+        var user = this.userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
+
+        user.setAtivo(false);
     }
 }
